dc723877a6825f87297c84a820a78910beec30ae
Sebastian Sitja <sebasitjamed@gmail.com>
2020-01-05 00:27:18 -0300

    Updates

---
+++ precache-manifest.d518199a94fa5ce13bf8fdcdd5c90e2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e0725b36a37c135f6288e54a9b7113",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "f1493fc83b6583c49115",
+    "url": "/react-deploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "00284b23efc8f2f2998c",
+    "url": "/react-deploy/static/js/2.13d22763.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-deploy/static/js/2.13d22763.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f1493fc83b6583c49115",
+    "url": "/react-deploy/static/js/main.c8b9b39e.chunk.js"
+  },
+  {
+    "revision": "620439153f0084079bf6",
+    "url": "/react-deploy/static/js/runtime-main.7fd0ee75.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
